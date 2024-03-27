c549c934df067eec98d53a62f1e9754082543702
Ruslan <saharcorez@gmail.com>
2024-03-27 18:06:06 +0300

    Положение виджета

--- phone-widget.js
+++ phone-widget.js
@@ -1,5 +1,18 @@
 window.addEventListener("DOMContentLoaded", function () {
+  const phoneWidget = document.querySelector(".phone-widget");
   const widgetLabel = document.querySelector(".phone-widget-c-sitephone-label");
+  const phoneWidgetBubble = phoneWidget && phoneWidget.querySelectorAll(".phone-widget-c-sitephone-label__bubble");
+  const phoneWidgetBackground = phoneWidget && phoneWidget.querySelectorAll(".phone-widget-c-sitephone-label__bubble--solid-color");
+
+  const color = phoneWidget.dataset.color;
+
+  phoneWidgetBubble.forEach((item) => {
+    item.style.border = `1px solid ${color}`;
+  })
+
+  phoneWidgetBackground.forEach((item) => {
+    item.style.backgroundColor = `${color}`;
+  })
 
   widgetLabel.classList.remove("phone-widget-js-sitephone-label--hidden");
   widgetLabel.classList.add("phone-widget-js-sitephone-label--shown");
