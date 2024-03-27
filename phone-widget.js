206c6d4a100a1977c4549cfeaa9cec9cb4bf6f68
Ruslan <saharcorez@gmail.com>
2024-03-27 15:10:14 +0300

    widget

---
+++ phone-widget.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const widgetLabel = document.querySelector(".phone-widget-c-sitephone-label");
+
+  widgetLabel.classList.remove("phone-widget-js-sitephone-label--hidden");
+  widgetLabel.classList.add("phone-widget-js-sitephone-label--shown");
+})
