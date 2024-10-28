c422fbff2de73541cd31c166f5db9b72ea2819b9
Ruslan <saharcorez@gmail.com>
2024-10-28 12:01:49 +0300

    feat: bottom

--- phone-widget.js
+++ phone-widget.js
@@ -5,6 +5,7 @@ window.addEventListener("DOMContentLoaded", function () {
   const phoneWidgetBackground = phoneWidget && phoneWidget.querySelectorAll(".phone-widget-label__bubble--solid-color");
 
   const color = phoneWidget.dataset.color;
+  const bottom = phoneWidget.dataset.bottom;
 
   phoneWidgetBubble.forEach((item) => {
     item.style.border = `1px solid ${color}`;
@@ -13,6 +14,10 @@ window.addEventListener("DOMContentLoaded", function () {
   phoneWidgetBackground.forEach((item) => {
     item.style.backgroundColor = `${color}`;
   })
+
+  if (phoneWidget.classList.contains("phone-widget_bottom")) {
+    widgetLabel.style.marginBottom = `${bottom}px`;
+  }
 
   widgetLabel.classList.remove("phone-widget-js-sitephone-label--hidden");
   widgetLabel.classList.add("phone-widget-js-sitephone-label--shown");
